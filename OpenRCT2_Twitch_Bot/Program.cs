75a14997f8412748eb99b7c13b889d751f1e6820
Krutonium <PFCKrutonium@gmail.com>
2018-06-19 18:08:36 -0400

    Add project files.

---
+++ OpenRCT2_Twitch_Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenRCT2_Twitch_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
